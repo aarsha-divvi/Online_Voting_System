82118cafa01c2fc2121b5445a44915b86b57c2c1
Aarsha Divvi <aarshadivvi@gmail.com>
2026-09-19 21:45:50 +0530

    Prepare application for cloud deployment

--- src/main/java/com/voting/online_voting_system/security/JwtService.java
+++ src/main/java/com/voting/online_voting_system/security/JwtService.java
@@ -3,6 +3,7 @@ package com.voting.online_voting_system.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -10,18 +11,22 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
-
 public class JwtService {
 
-    private static final String SECRET_KEY =
-            "OnlineVotingSystemSecretKeyForJWT2026Secure";
+    private final String secretKey;
 
     private static final long EXPIRATION_TIME =
-            1000 * 60 * 60; // 1 hour
+            1000 * 60 * 60;
+
+    public JwtService(
+            @Value("${jwt.secret}") String secretKey
+    ) {
+        this.secretKey = secretKey;
+    }
 
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(
-                SECRET_KEY.getBytes(StandardCharsets.UTF_8)
+                secretKey.getBytes(StandardCharsets.UTF_8)
         );
     }
 
@@ -32,11 +37,15 @@ public class JwtService {
                 .claim("role", role)
                 .issuedAt(new Date())
                 .expiration(
-                        new Date(System.currentTimeMillis() + EXPIRATION_TIME)
+                        new Date(
+                                System.currentTimeMillis()
+                                        + EXPIRATION_TIME
+                        )
                 )
                 .signWith(getSigningKey())
                 .compact();
     }
+
     public String extractRole(String token) {
 
         Claims claims = Jwts.parser()
@@ -47,6 +56,7 @@ public class JwtService {
 
         return claims.get("role", String.class);
     }
+
     public String extractVoterId(String token) {
 
         Claims claims = Jwts.parser()
@@ -58,7 +68,10 @@ public class JwtService {
         return claims.getSubject();
     }
 
-    public boolean isTokenValid(String token, String voterId) {
+    public boolean isTokenValid(
+            String token,
+            String voterId
+    ) {
 
         try {
             Claims claims = Jwts.parser()
